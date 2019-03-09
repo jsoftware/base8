83e7061efb641afb3c4c70034129ce160224b58e
bill lam <bbill.lam@gmail.com>
2019-03-09 13:05:34 +0800

    add ijconsole.sh to install-usr.sh

--- config/install-usr.sh
+++ config/install-usr.sh
@@ -47,18 +47,22 @@ cp bin/profile.ijs /etc/j/8.07/.
 cp bin/profilex_template.ijs /etc/j/8.07/.
 find /etc/j/8.07 -type d -exec chmod a+rx {} \+
 find /etc/j/8.07 -type f -exec chmod a+r {} \+
+echo "#!/bin/bash" > ijconsole.sh
+echo "cd \"\$HOME\" && /usr/bin/ijconsole" >> ijconsole.sh
+mv ijconsole.sh /usr/bin/.
+chmod 755 /usr/bin/ijconsole.sh
 cp bin/jconsole /usr/bin/ijconsole-8.07
-chmod a+rx /usr/bin/ijconsole-8.07
+chmod 755 /usr/bin/ijconsole-8.07
 (cd /usr/bin && ln -sf ijconsole-8.07 ijconsole)
 
 if [ "$cpu" = "intel64" ] ; then
  if [ -d "/usr/lib/x86_64-linux-gnu" ] ; then
   cp bin/libj.so /usr/lib/x86_64-linux-gnu/libj.so.8.07
-  chmod a+r /usr/lib/x86_64-linux-gnu/libj.so.8.07
+  chmod 644 /usr/lib/x86_64-linux-gnu/libj.so.8.07
  else
   if [ -d "/usr/lib64" ] ; then
    cp bin/libj.so /usr/lib64/libj.so.8.07
-   chmod a+r /usr/lib64/libj.so.8.07
+   chmod 644 /usr/lib64/libj.so.8.07
   else
    echo "can not find lib directory"
    exit 1
@@ -68,11 +72,11 @@ fi
 if [ "$cpu" = "arm64" ] ; then
  if [ -d "/usr/lib/aarch64-linux-gnu" ] ; then
   cp bin/libj.so /usr/lib/aarch64-linux-gnu/libj.so.8.07
-  chmod a+r /usr/lib/aarch64-linux-gnu/libj.so.8.07
+  chmod 644 /usr/lib/aarch64-linux-gnu/libj.so.8.07
  else
   if [ -d "/usr/lib64" ] ; then
    cp bin/libj.so /usr/lib64/libj.so.8.07
-   chmod a+r /usr/lib64/libj.so.8.07
+   chmod 644 /usr/lib64/libj.so.8.07
   else
    echo "can not find lib directory"
    exit 1
@@ -82,11 +86,11 @@ fi
 if [ "$cpu" = "intel32" ] ; then
  if [ -d "/usr/lib/i386-linux-gnu" ] ; then
   cp bin/libj.so /usr/lib/i386-linux-gnu/libj.so.8.07
-  chmod a+r /usr/lib/i386-linux-gnu/libj.so.8.07
+  chmod 644 /usr/lib/i386-linux-gnu/libj.so.8.07
  else
   if [ -d "/usr/lib" ] ; then
    cp bin/libj.so /usr/lib/libj.so.8.07
-   chmod a+r /usr/lib/libj.so.8.07
+   chmod 644 /usr/lib/libj.so.8.07
   else
    echo "can not find lib directory"
    exit 1
@@ -96,11 +100,11 @@ fi
 if [ "$cpu" = "arm32" ] ; then
  if [ -d "/usr/lib/arm-linux-gnueabihf" ] ; then
   cp bin/libj.so /usr/lib/arm-linux-gnueabihf/libj.so.8.07
-  chmod a+r /usr/lib/arm-linux-gnueabihf/libj.so.8.07
+  chmod 644 /usr/lib/arm-linux-gnueabihf/libj.so.8.07
  else
   if [ -d "/usr/lib" ] ; then
    cp bin/libj.so /usr/lib/libj.so.8.07
-   chmod a+r /usr/lib/libj.so.8.07
+   chmod 644 /usr/lib/libj.so.8.07
   else
    echo "can not find lib directory"
    exit 1
